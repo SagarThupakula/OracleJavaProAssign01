111473832c52d0dab3b40b3ce244c26bb2877dc0
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-07 11:47:41 +0530

    Sagar

---
+++ OracleJavaProAssign001/src/Test2.java
@@ -0,0 +1,12 @@
+
+public class Test2 {
+		void student1() {
+			System.out.println("T.Sagar,CSE-A,18H65A0505");
+		}
+		void student2() {
+			System.out.println("Sudheer,CSE-A,17H61a024");
+			}
+		void student3() {
+			System.out.println("Rohith,CSE-A,17H61A0557");
+			}
+		}
---
+++ OracleJavaProAssign001/src/TestDriver.java
@@ -0,0 +1,39 @@
+
+
+public class TestDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Test1 t1=new Test1();
+Test2 t2=new Test2();
+Test3 t3=new Test3();
+Test4 t4=new Test4();
+Test5 t5=new Test5();
+System.out.println("student1.java output");
+t1.student1();
+t1.student2();
+t1.student3();
+System.out.println("######################");
+System.out.println("student2.java output");
+t2.student1();
+t2.student2();
+t2.student3();
+System.out.println("######################");
+System.out.println("student3.java output");
+t3.student1();
+t3.student2();
+t3.student3();
+System.out.println("######################");
+System.out.println("student1.java output");
+t4.student1();
+t4.student2();
+t4.student3();
+System.out.println("######################");
+System.out.println("student1.java output");
+t5.student1();
+t5.student2();
+t5.student3();
+System.out.println("######################");
+	}
+
+}
